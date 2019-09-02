f1b774aa50a688baf491837a8f153fde00967c44
unknown <giovanni.hauber@gmail.com>
2019-09-02 17:48:50 +0200

    Initial commit

---
+++ GTA5NetworkManager/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace MainWindowApp {
+    /// <summary>
+    /// Some Logic
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        //manager for blocking/ublocking the ports
+        GTA5Session manager = new GTA5Session();
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void BlockButton_Click(object sender, RoutedEventArgs e) {
+
+            //blocking this button 
+            BlockButton.IsEnabled = false;
+
+            //checking if the disable button is disabled; if so, enable it
+            if (!UnblockButton.IsEnabled) UnblockButton.IsEnabled = true;
+
+            //Using the block method on the manager 
+            manager.Block();
+            
+            //Chaning the status to active
+            ActiveLabel.Content = "Active!";
+            ActiveLabel.Foreground = Brushes.Green;
+          
+        }
+
+        private void UnblockButton_Click(object sender, RoutedEventArgs e) {
+
+            //blocking this button 
+            UnblockButton.IsEnabled = false;
+
+            //checking if the enable button is disabled; if so, enable it
+            if (!BlockButton.IsEnabled) BlockButton.IsEnabled = true;
+
+            //Using the unblock method on the manager
+            manager.Unblock();
+
+            //Changing the status to not active
+            ActiveLabel.Content = "Not Active!";
+            ActiveLabel.Foreground = Brushes.Red;
+        }
+
+        private void HelpButton_Click(object sender, RoutedEventArgs e) {
+
+            //Show the credit and help messagebox. Store the result of the visit yes/no into the boxResult
+            MessageBoxResult boxResult = MessageBox.Show(
+            "Usage:\n" +
+            "Enable solo session before running GTA Online, or Enable solo session while in-game and go to 'Online -> Find a new session' to play alone.\n\n" +
+            "Command Help:\n" +
+            "Enable: enable the solo session.\n" +
+            "Disable: let you join into populate session again\n\n" +
+            "Credit:\n" +
+            "Author: Asynchronousx\n" +
+            "Site: https://github.com/Asynchronousx) \n" +
+            "Visit the site?",
+            "Help & Credit",
+            MessageBoxButton.YesNo);
+
+           //if the answer of the box result is yes, open the website
+           if(boxResult == MessageBoxResult.Yes) {
+                System.Diagnostics.Process.Start("https://github.com/Asynchronousx");
+            }
+
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e) {
+            System.Environment.Exit(1);
+        }
+
+
+        //Method that allow the user to drag the borderless form while pressing the mouse's left button.
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e) {
+            base.OnMouseLeftButtonDown(e);
+
+            // Begin dragging the window
+            this.DragMove();
+        }
+
+    }
+}
---
+++ GTA5NetworkManager/class/GTA5Manager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NetFwTypeLib;
+
+namespace MainWindowApp { 
+    public class GTA5Session {
+
+        //Global univoque identifier (guid) necessary to identify the firewall object class.
+        //Those identifier are necessary to ask the firewall to add our custom rules
+        const string guidFWPolicy2 = "{E2B3C97F-6AE1-41AC-817A-F6F92166D7DD}";
+        const string guidRWRule = "{2C5BC43E-3369-4C33-AB0C-BE9469677AF4}";
+
+        //Creating the Object needed later on to work on the new rule and apply that rule
+        //on the firewall
+        Type typeFWPolicy2 = Type.GetTypeFromCLSID(new Guid(guidFWPolicy2));
+        Type typeFWRule = Type.GetTypeFromCLSID(new Guid(guidRWRule));
+
+        public void Block() {
+            //Creating the istances of the fwPolicy and newRule.
+            //fwPolicy is the actual Firewall rules activator, newRule contains all the rules
+            //necessary to the program to block successfully the PORT on which the GTA Servers 
+            //rely on
+            INetFwPolicy2 fwPolicy2 = (INetFwPolicy2)Activator.CreateInstance(typeFWPolicy2);
+            INetFwRule newRule = (INetFwRule)Activator.CreateInstance(typeFWRule);
+
+            //Setting the rules property 
+            newRule.Name = "GTA5 Solo Public Session";
+            newRule.Description = "Block some ports to get into an online session alone";
+
+            //Since the ports we're working on rely on the UDP protocol, we need to specify that
+            newRule.Protocol = (int)NET_FW_IP_PROTOCOL_.NET_FW_IP_PROTOCOL_UDP;
+
+            //Ports that will be blocked for the sake of the solo sessions
+            newRule.LocalPorts = "6672,61455,61457,61456,61458";
+
+            //We're blocking those ports only by the outbond traffic.
+            newRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT;
+
+            //remaining stuff: setting it active, specifing the grouping procotol, setting the current
+            //using fw profile and setting the action to BLOCK, to block the outbond traffic
+            newRule.Enabled = true;
+            newRule.Grouping = "@firewallapi.dll,-23255";
+            newRule.Profiles = fwPolicy2.CurrentProfileTypes;
+            newRule.Action = NET_FW_ACTION_.NET_FW_ACTION_BLOCK;
+
+            //adding the new rule created to the firewall policy obj: that will enable the solo session
+            fwPolicy2.Rules.Add(newRule);
+        }
+
+        public void Unblock() { 
+            //generating another fwpolicy
+            INetFwPolicy2 fwPolicy2 = (INetFwPolicy2)Activator.CreateInstance(typeFWPolicy2);
+
+            //removing the rules created with the Block method.
+            fwPolicy2.Rules.Remove("GTA5 Solo Public Session");
+        }
+    }
+}
