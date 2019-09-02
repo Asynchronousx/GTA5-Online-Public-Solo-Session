e832bb8e275b9d5075a85b1431e43d333610cafd
unknown <giovanni.hauber@gmail.com>
2019-09-02 18:08:56 +0200

    added exe icon

--- GTA5NetworkManager/obj/Debug/GTA5NetworkManager_Content.g.i.cs
+++ GTA5NetworkManager/obj/Debug/GTA5NetworkManager_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/stand-by.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("pssv.ico")]
 
 
